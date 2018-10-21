bbb21a2c08dd8e83f20db464795dccefa8cf9632
futurist <26634873@qq.com>
2018-10-21 22:38:19 +0800

    update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -24,7 +24,7 @@ declare interface IUnwrapConfig {
 
 declare interface IChangeStreamValue {
     value: IWrappedData,
-    type: 'change'|'delete'|'add',
+    type: 'change' | 'delete' | 'add',
     path: string[]
 }
 
@@ -36,14 +36,16 @@ declare interface IWrappedData extends Stream<any> {
         path: string | string[],
         filter?: (data: IChangeStreamValue) => boolean,
         wrapper?: IWrappedData
-    ): IWrappedData | any;
-    get(path: string | string[]): IWrappedData | any;
+    ): IWrappedData | undefined;
+    get(path: string | string[], mapFunc?: (val: IWrappedData | undefined) => any): IWrappedData | any;
     set(value: any): IWrappedData;
     set(path: string | string[], value: any, descriptor?: object): IWrappedData;
-    getset(valueFn: (prevVal: IWrappedData | any)=>any): IWrappedData;
-    getset(path: string | string[], valueFn: (prevVal: IWrappedData | any)=>any, descriptor?: object): IWrappedData;
+    getMany(pathMap: object | string[] | string, mapFunc?: (val: IWrappedData | undefined) => any): object | any[] | any;
+    setMany(kvMap: object, descriptors?: object): void;
+    getset(valueFn: (prevVal: IWrappedData | undefined) => any): IWrappedData;
+    getset(path: string | string[], valueFn: (prevVal: IWrappedData | undefined) => any, descriptor?: object): IWrappedData;
     ensure(path: string | string[], value: any, descriptor?: object): IWrappedData;
-    ensure(invalid: (prevVal: IWrappedData)=>boolean, path: string | string[], value: any, descriptor?: object): IWrappedData;
+    ensure(invalid: (prevVal: IWrappedData) => boolean, path: string | string[], value: any, descriptor?: object): IWrappedData;
     unset(path: string | string[]): any;
     unwrap(config?: IUnwrapConfig): any;
     unwrap(path: string | string[], config?: IUnwrapConfig): any;
