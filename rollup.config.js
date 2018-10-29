2944fbd7bdf64560c51daa1e7b3a8beed9b76db7
futurist <26634873@qq.com>
2018-10-29 15:53:25 +0800

    commonjs add buble

--- rollup.config.js
+++ rollup.config.js
@@ -4,28 +4,28 @@ import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 
 export default [
-    {
-        input: './src/index.js',
-        plugins: [
-            buble(),
-            resolve(),
-            commonjs()
-        ],
-        output: [
-            { format: 'umd', file: 'dist/index.umd.js', name: 'wrapData' },
-        ]
-    },
+  {
+    input: './src/index.js',
+    plugins: [
+      buble(),
+      resolve(),
+      commonjs()
+    ],
+    output: [
+      { format: 'cjs', file: 'dist/index.cjs.js' },
+      { format: 'umd', file: 'dist/index.umd.js', name: 'wrapData' }
+    ]
+  },
 
-    {
-        input: './src/index.js',
-        plugins: [
-            resolve(),
-            commonjs()
-        ],
-        output: [
-            { format: 'cjs', file: 'dist/index.cjs.js' },
-            { format: 'es', file: 'dist/index.es.js' }
-        ]
-    },
+  {
+    input: './src/index.js',
+    plugins: [
+      resolve(),
+      commonjs()
+    ],
+    output: [
+      { format: 'es', file: 'dist/index.es.js' }
+    ]
+  }
 
 ]
